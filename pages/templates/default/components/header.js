1061db60912e64873fac6fe73aa9b05e2028d72e
jkga <johnkennethgibasabella@gmail.com>
2020-04-22 15:06:04 +0800

    fix: header props in build env

--- pages/templates/default/components/header.js
+++ pages/templates/default/components/header.js
@@ -2,11 +2,14 @@ import { Container, Label } from 'semantic-ui-react'
 import styles from '../style.module.css'
 
 export default (props) => {
+  // prevent prop building and read-only error
+  let prop = { ...props } /* eslint prefer-const: */
+  prop.profile = prop.profile || { basics: {} }
   return (
     <header className={styles.mainHeader}>
       <Container>
         <Label>
-          {props.profile.basics.name} / <Label.Detail as='a' href={props.profile.basics.website} target='_blank' rel='noopener norefer'>{props.profile.basics.website}</Label.Detail>
+          {prop.profile.basics.name} / <Label.Detail as='a' href={prop.profile.basics.website} target='_blank' rel='noopener norefer'>{prop.profile.basics.website}</Label.Detail>
         </Label>
       </Container>
     </header>)
